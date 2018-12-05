e2645913be2338e209d20a3c898511690fb8cb07
kvarun10@gmail.com <Varun Kashyap>
2018-12-06 01:21:00 +0530

    final merge start

--- projects/employeer/src/lib/services/job-application.service.ts
+++ projects/employeer/src/lib/services/job-application.service.ts
@@ -10,18 +10,12 @@ export class JobApplicationService {
   constructor(private http : HttpClient) { }
 
   //private userUrl = '/job-application';
-<<<<<<< HEAD
-  private userUrl:string="http://localhost:8765/seeker-service";
-=======
   private userUrl:string="http://localhost:8765/job-application";
->>>>>>> 5e4c10673dfe32401beac126aa2144b75481cdd1
 
   public postJobApplication(JobApplication){
-    console.log("POSTING JOB");
     return this.http.post<JobApplication>(this.userUrl+"/employer/postjobs/job", JobApplication)
   }
   public getJobApplication(id:string){
-
     return this.http.get<JobApplication[]>(this.userUrl+"/employer/seejobs/"+id)
   }
   public deleteJobApplication(jobId:string){
